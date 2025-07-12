015c1d18b0ed57bbed408f8c7c8f35ea39a28acb
Luis Zambrano <luisezamb@gmail.com>
2025-07-11 21:13:17 -0400

    doing examples

---
+++ exercise/01/variables/src/main.rs
@@ -0,0 +1,10 @@
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
+
+fn main() {
+    let (mut missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+    let _mono: i32 = 9;
+    missiles = missiles - ready;
+
+    println!("Firing {} of my {} missiles...", ready, missiles);
+}
